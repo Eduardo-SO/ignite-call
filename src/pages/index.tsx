cc397ce5896fdca55aa8b8b37cde0826f862cf61
Eduardo S.O <eduardo.sool07@gmail.com>
2023-04-14 12:33:29 -0300

    Add ESLint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { Heading } from "@izuka/react";
+import { Heading } from '@izuka/react'
 
 export default function Home() {
-  return (
-    <Heading as="h1">Hello Heading</Heading>
-  )
+  return <Heading as="h1">Hello Heading</Heading>
 }
